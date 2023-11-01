72fcf5b962bb586b12229adb7eb5c243331cd54e
ThyR6 <ThyR.Han7@gmail.com>
2023-11-01 21:46:46 +0500

    Added Some New Functions. May Need To Be Optimized

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,12 +1,23 @@
 from os import system
 
 class TicTacToe:
-	def showGame(self):
-		for i in range(3):
-			for j in range(3):
-				if j%3==2: print(3*i+j+1,end=" ")
-				else: print(3*i+j+1,end=" | ")
-			if i%3!=2: print("\n---------")
+	# 3*i+j+1
+	def showGame(self,arr1=None,arr2=None,OX=True):
+		if not OX:
+			for i in range(3):
+				for j in range(3):
+					if(arr1!=None):
+						if j%3==2: print(arr1[3*i+j],end=" ")
+						else: print(arr1[3*i+j],end=" | ")
+				print("\t\t\t",end="")
+				# if i%3!=2: print("\t\t\t---------",end="")
+				for j in range(3):
+					if(arr2!=None):
+						if j%3==2: print(arr2[3*i+j],end=" ")
+						else: print(arr2[3*i+j],end=" | ")
+				if i%3!=2 and arr2!=None: print("\n---------\t\t\t---------")
+				elif i%3!=2: print("\n---------")
+		
 
 	def ToClassic(self,num):
 		if num>6: return num-6
@@ -47,7 +58,9 @@ class TicTacToe:
 
 		while(self.Mode=="?"):
 			self.Welcome()
-			self.Mode=input("Which Mode Do You Want To Play: \nA: Classic\nB: Modern\nEnter Your Choice: ")
+			print("Which Mode Do You Want To Play: ")
+			self.showGame([1,2,3,4,5,6,7,8,9],[7,8,9,4,5,6,1,2,3],OX=False)
+			self.Mode=input("\nA: Classic\t\t\tB: Modern\nEnter Your Choice: ")
 			if(self.Mode in ["A","a","C","c","1"]): self.Mode="C"
 			elif(self.Mode in ["B","b","M","m","2"]): self.Mode="M"
 			else: self.Mode="?"
@@ -61,8 +74,47 @@ class TicTacToe:
 				elif(self.OX in ["B","b","X","x","2"]): self.OX="X"
 				else: self.OX="?"
 				system("cls")
+	
+	def Input(self):
+		i=0
+		for index,num in enumerate(self.Game):
+			if(num==0):
+				i=index
+				break
+		self.Game[i]=int(input("Which Maneuver You Want To Perform: "))
+		
+	def UpdateStatus(self):
+		pass
+
+	def StartGame(self):
+		self.Game=[0,0,0,0,0,0,0,0,0]
+		self.Status=0 # 	0 -> Ongoing;	1 -> X Wins;	2 -> O Wins;	3 -> Draw;
+		system("cls")
+		self.GetOpts()
+		while(self.Status==0):
+			self.Welcome()
+			self.showGame(self.Game)
+			print("\n")
+			self.Input()
+			self.UpdateStatus()
+		self.Welcome()
+		match self.Status:
+			case 1: print("'X' has Won The Game")
+			case 2: print("'O' has Won The Game")
+			case 3: print("The Game Has Ended In Draw")
+
+	def ToOX(self,index):
+		for i in range(9):
+			if (self.Game[i]==index):
+				if(i%2): return "X"
+				return "O"
+			return " "
+			
+
 
 
 TicTac = TicTacToe()
-TicTac.GetOpts()
-TicTac.showGame()
+# TicTac.GetOpts()
+# TicTac.showGame([7,8,9,4,5,6,1,2,3],[1,2,3,4,5,6,7,8,9])
+# TicTac.showGame([7,8,9,4,5,6,1,2,3])
+TicTac.StartGame()
--- readme.md
+++ readme.md
@@ -2,7 +2,11 @@
 The ThyR. Han's Implementation of Tic-Tac-Toe
 
 $ git init
+
 $ git add -A
+
 $ git commit -a -m "."
+
 $ git remote add origin https://github.com/ThyR6/TicTacToe.git
-$ git push --set-upstream origin master
+
+$ git push --set-upstream origin main
