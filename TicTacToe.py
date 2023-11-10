1e5635ba70a78222b604f1cda8df80f67e9abe1a
ThyR6 <ThyR.Han7@gmail.com>
2023-11-10 20:15:49 +0500

    Basic AI made

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,40 +1,32 @@
 from os import system
+import numpy as np
 
 class TicTacToe:
 
 	def showGame(self,arr1=None,arr2=None,OX=False):
-		n=int(self.Level)
-		for i in range(n):
-			for j in range(n):
+		for i in range(3):
+			for j in range(3):
 				if(arr1!=None):
-					if j%n==n-1: print(self.ToOX(n*i+j+1),end=" ")
-					else: print(self.ToOX(n*i+j+1),end=" | ")
+					if j%3==2: print(self.ToOX(3*i+j+1),end=" ")
+					else: print(self.ToOX(3*i+j+1),end=" | ")
 				elif OX==True: pass
 				else:
-					if self.Level=="3":
-						if j%n==n-1: print(n*i+j+1,end=" ")
-						else: print(n*i+j+1,end=" | ")
-					else:
-						if j%n==n-1: print(i+1,j+1,end=" ",sep="")
-						else: print(i+1,j+1,end=" | ",sep="")
+					if j%3==2: print(3*i+j+1,end=" ")
+					else: print(3*i+j+1,end=" | ")
 			print("\t\t\t",end="")
 
-			for j in range(n):
+			for j in range(3):
 				if(arr2!=None):
-					if j%n==n-1: print(self.ToOX(n*i+j+1),end=" ")
-					else: print(self.ToOX(n*i+j+1),end=" | ")
+					if j%3==2: print(self.ToOX(3*i+j+1),end=" ")
+					else: print(self.ToOX(3*i+j+1),end=" | ")
 				elif OX==True: pass
 				else:
-					if self.Level=="3":
-						if j%n==n-1: print(n*i+j+1,end=" ")
-						else: print(n*i+j+1,end=" | ")
-					else:
-						if j%n==n-1: print(i+1,j+1,end=" ",sep="")
-						else: print(i+1,j+1,end=" | ",sep="")
+					if j%3==2: print(self.ToClassic(3*i+j+1),end=" ")
+					else: print(self.ToClassic(3*i+j+1),end=" | ")
 
-			if i%n!=n-1:
-				if OX==False and self.Level!="3": print("\n","-----"*n,"\b\b\b\t\t\t","-----"*n,sep="")
-				else: print("\n","----"*n,"\b\b\b   ",sep="")
+			if i%3!=3-1:
+				if OX==False: print("\n----------\t\t\t----------",sep="")
+				else: print("\n----------",sep="")
 
 	def ToClassic(self,num):
 		if num>6: return num-6
@@ -46,7 +38,6 @@ class TicTacToe:
 
 	def GetOpts(self):
 		self.P2="?"
-		self.Level="?"
 		self.Mode="?"
 		self.OX="?"
 		while(self.P2=="?"):
@@ -58,31 +49,16 @@ class TicTacToe:
 			else: self.P2="?"
 			system("cls")
 
-		while(self.Level=="?"):
-			self.Welcome()
-			self.Level=input("Which Difficulty Level Do You Want To Play: \nA: Basic(3x3)\nB: Advanced(4x4)\nC: Custom\nEnter Your Choice: ")
-			if(self.Level in ["A","a","3"]): self.Level="3"
-			elif(self.Level in ["B","b","4"]): self.Level="4"
-			elif(self.Level in ["C","c","3"]): self.Level="C"
-			else: self.Level="?"
-			system("cls")
 
-		while(self.Level=="C"):
+		while(self.Mode=="?"):
 			self.Welcome()
-			self.Level=input("How Many Rows/Columns Do You Want To Have: \nEnter Your Choice: ")
-			if(self.Level not in ["3","4","5","6","7","8","9"]): self.Level="C"
+			print("Which Mode Do You Want To Play: ")
+			self.showGame()
+			self.Mode=input("\nA: Classic\t\t\tB: Modern\nEnter Your Choice: ")
+			if(self.Mode in ["A","a","C","c","1"]): self.Mode="C"
+			elif(self.Mode in ["B","b","M","m","2"]): self.Mode="M"
+			else: self.Mode="?"
 			system("cls")
-
-		if self.Level =="3":
-			while(self.Mode=="?"):
-				self.Welcome()
-				print("Which Mode Do You Want To Play: ")
-				self.showGame()
-				self.Mode=input("\nA: Classic\t\t\tB: Modern\nEnter Your Choice: ")
-				if(self.Mode in ["A","a","C","c","1"]): self.Mode="C"
-				elif(self.Mode in ["B","b","M","m","2"]): self.Mode="M"
-				else: self.Mode="?"
-				system("cls")
 
 		if(self.P2=="B"):
 			while(self.OX=="?"):
@@ -100,24 +76,38 @@ class TicTacToe:
 			if(num==0):
 				i=index
 				break
-		while(1):
-			try:
-				tmp=int(input("Which Maneuver You Want To Perform: "))
-				break
-			except: 
-				pass
+		if(self.P2=="B" and self.OX=="X" and self.Game[0]==0):
+			tmp=[1,1,3,3,5,7,7,9,9][np.random.randint(0,9)]
+		elif(self.P2=="B" and i%2==0 and self.OX=="X"):
+			print("ThyR. AI is thinking about next move... ",end="")
+			tmp=self._AI(self.Game)
+		elif(self.P2=="B" and i%2==1 and self.OX=="O"):
+			print("ThyR. AI is thinking about next move... ",end="")
+			tmp=self._AI(self.Game)
+		else:
+			while(1):
+				try:
+					if(self.Mode=="M"): tmp=self.ToClassic(int(input("Which Maneuver You Want To Perform: ")))
+					else: tmp=int(input("Which Maneuver You Want To Perform: "))
+					break
+				except:
+					pass
 		if(tmp not in self.Game):
-			if(self.Mode=="M"): self.Game[i]=self.ToClassic(tmp)
-			else: self.Game[i]=tmp
+			self.Game[i]=tmp
 
-	def UpdateStatus(self):
-		self.G=[self.Game[1::2],self.Game[0::2]]
+	def _Status(self,GameL): # 	0 -> Ongoing;	1 -> X Wins;	2 -> O Wins;	3 -> Draw;
+		Status=0
+		if(GameL[8]!=0): Status=3
+		G=[GameL[1::2],GameL[0::2]]
 		for P in range(2):
 			for i in range(3):
-				if all(3*i+j in self.G[P] for j in range(1,4)): self.Status=P+1
-				if all(i+j in self.G[P] for j in range(1,9,3)): self.Status=P+1
-				if all(i in self.G[P] for i in [1,5,9]) or all(i in self.G[P] for i in [3,5,7]): self.Status=P+1
-		if(self.Game[8]!=0): self.Status=3
+				if all(3*i+j in G[P] for j in range(1,4)): Status=P+1
+				if all(i+j in G[P] for j in range(1,9,3)): Status=P+1
+				if all(i in G[P] for i in [1,5,9]) or all(i in G[P] for i in [3,5,7]): Status=P+1
+		return Status
+
+	def UpdateStatus(self):
+		self.Status=self._Status(self.Game)
 
 	def StartGame(self):
 		self.Game=[0,0,0,0,0,0,0,0,0]
@@ -125,11 +115,13 @@ class TicTacToe:
 		system("cls")
 		self.GetOpts()
 		while(self.Status==0):
+			system("cls")
 			self.Welcome()
 			self.showGame(self.Game,OX=True)
 			print("\n")
 			self.Input()
 			self.UpdateStatus()
+		system("cls")
 		self.Welcome()
 		self.showGame(self.Game,OX=True)
 		match self.Status:
@@ -144,11 +136,62 @@ class TicTacToe:
 				return "O"
 		return " "
 	
+	def AI(self,GameAI):
+		_0=0
+		for i in range(9):
+			if GameAI[i]==0:
+				_0=i+1
+				break
+		if self._Status(GameAI)==1: return 1*(9-_0)
+		if self._Status(GameAI)==2: return -1*(9-_0)
+		if self._Status(GameAI)==3: return 0
+
+		GameL=[]
+		# tmp=[]
+		for i in range(1,10):
+			if i not in GameAI: 
+				tmp=GameAI.copy()
+				tmp[_0-1]=i
+				GameL.append(self.AI(tmp))
+				
+		if(_0%2==0): return max(GameL)
+		else: return min(GameL)
+
+	def _AI(self,GameAI):
+		_0=0
+		for i in range(9):
+			if GameAI[i]==0:
+				_0=i+1
+				break
+		if self._Status(GameAI)!=0: return 0
+
+		GameL=[[],[]]
+		# tmp=[]
+		for i in range(1,10):
+			if i not in GameAI: 
+				tmp=GameAI.copy()
+				tmp[_0-1]=i
+				GameL[0].append(i)
+				GameL[1].append(self.AI(tmp))
+		minimax=[GameL[1][0]]
+		idx=[GameL[0][0]]
+		for i in range(1,len(GameL[1])):
+			if(_0%2==0 and GameL[1][i]>minimax[0]):
+				minimax=[GameL[1][i]]
+				idx=[GameL[0][i]]
+			elif(_0%2==1 and GameL[1][i]<minimax[0]):
+				minimax=[GameL[1][i]]
+				idx=[GameL[0][i]]
+			elif(_0%2==0 and GameL[1][i]==minimax[0]):
+				minimax.append(GameL[1][i])
+				idx.append(GameL[0][i])
+			elif(_0%2==1 and GameL[1][i]==minimax[0]):
+				minimax.append(GameL[1][i])
+				idx.append(GameL[0][i])
+
+		return idx[np.random.randint(0, len(idx))]
 
 
 
 TicTac = TicTacToe()
-# TicTac.GetOpts()
-# TicTac.showGame([7,8,9,4,5,6,1,2,3],[1,2,3,4,5,6,7,8,9])
-# TicTac.showGame([7,8,9,4,5,6,1,2,3])
 TicTac.StartGame()
